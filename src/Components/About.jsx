dcd985aba4ebf0ffd755dd43f94130f48376405f
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-11-04 09:07:32 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -23,19 +23,31 @@ const imageAltText = "purple and blue abstract background";
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description = "I'm Dineth Siriwardana, an undergraduate specializing in Information and Communication Technology at the Faculty of Technology, University of Ruhuna. My interests include creative problem-solving, leadership, and pressure-handling. You can connect with me on LinkedIn: [Dineth Siriwardana](https://www.linkedin.com/in/dinethsiriwardana/).";
+const description =
+  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
+/**
+ * List of some of skills or technologies you work on, are learning,
+ * passionate about, or enjoy,
+ */
 const skillsList = [
   "Full Stack Developer",
   "Flutter Expert",
   "Inclusive design",
   "Focus group testing",
   "Mobile user interfaces",
-  "Graphic design",s
+  "Graphic design",
 ];
 
-const detailOrQuote = "I'm passionate about solving problems in new creative ways to drive innovation. Leveraging my UI/UX experience, I continually seek new and better ways to make technology accessible to all.";
-bout = () => {
+/**
+ * Use this to give more information about what you are passionate about,
+ * how you best work, or even a quote. This will help someone learn more
+ * about you on a professional level.
+ */
+const detailOrQuote =
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+
+const About = () => {
   return (
     <section className="padding" id="about">
       <img className="background" src={image} alt={imageAltText} />
