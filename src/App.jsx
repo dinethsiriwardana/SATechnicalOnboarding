844aec01f7291cd1e654493902b7e8cbba76c954
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-11-04 08:05:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Dineth Siriwardana",
+  title: "Full Stack Developer",
+  email: "dinethsiriwardana28@gmail.com", 
+  gitHub: "dinethsiriwardana",
+  instagram: "dineth_siriwardana",
+  linkedIn: "dinethsiriwardana",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "dinethsiri",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
